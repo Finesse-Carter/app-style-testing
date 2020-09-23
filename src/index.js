6116219e2d21163feb83ad2fd1ea84f85ca4d7e3
Finesse-Carter <shop.scrxvms@gmail.com>
2020-09-23 07:52:50 -0400

    got ride of

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
-import Header from './Header';
 
 render(
   <React.StrictMode>
     <App />
-    <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
