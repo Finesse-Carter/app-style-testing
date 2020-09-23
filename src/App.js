bf7886ff4b7cf5dcd312a065a9a41235689ddcd5
Finesse-Carter <shop.scrxvms@gmail.com>
2020-09-23 07:51:54 -0400

    added parts and course

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Header from './Header';
 
 
 const App = () =>{
-
+  const course = 'Half Stack application development'
+  const part = [
+     {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  },
+  ]
   return(
   <div className="App">
+<Header course={course}/>
      </div>
 )
 }
