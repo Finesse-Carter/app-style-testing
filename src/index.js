2268bed816371f51765806e11d8e626488d7a927
Finesse-Carter <shop.scrxvms@gmail.com>
2020-09-23 07:33:48 -0400

    added a test header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
+import Header from './Header';
 
 render(
   <React.StrictMode>
     <App />
+    <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
