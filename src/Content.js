d8302b168c90c2e15edae0a48789cb47795cf3aa
Finesse-Carter <shop.scrxvms@gmail.com>
2020-12-08 20:49:46 -0500

    name

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,13 @@
 import React from "react";
-const Content = () =>{
 
-    // console.log(props)
-    const part = ['Fundamentals of React','Using props to pass data','State of a component']
-    const names =part.map(element => <p> {element}</p>);
-    console.log(names);
+const Content = (prop) =>{
 
+    
     return(
         <div className='content' >
- {names}
+ <p>
+  
+ </p>
         </div>
     )
 }
